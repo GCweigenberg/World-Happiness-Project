b064c3eaf522991e24f38400f448c643772161b7
nkeplinger <keplinger.nicolette@gmail.com>
2023-02-23 20:52:20 -0600

    flast api to host jsons

--- app/app.py
+++ app/app.py
@@ -2,7 +2,10 @@
 #FLASK IS SETTING UP THE WEBSITE
 #FLASK WILL LOOK FOR TEMPLATES IN A TEMPLATES FOLDER!!! ASSUMED FOLDER STRUCTURE
 from flask import Flask, render_template
+import os
+from flask import Flask, render_template, url_for, json
 import pandas as pd
+import json
 app = Flask(__name__)
 
 
@@ -11,7 +14,44 @@ def root():
     return render_template('index.html')
     
     #If we want a button on the homepage
-    #return '<h1> Hello  <button> I am Button! </button> </h1>'  
+    #return '<h1> Hello  <button> I am Button! </button> </h1>'
+#@app.route('/data_2015') 
+#Run function below at webpage route above
+@app.route('/data')
+def get_json():
+    f = open('json_data/data.json')
+    data = json.load(f)
+    return {"data" : data}
+
+@app.route('/data_2015')
+def get_json():
+    f = open('json_data/json_2015')
+    data = json.load(f)
+    return {"data" : data}
+
+@app.route('/data_2016')
+def get_json():
+    f = open('json_data/json_2016')
+    data = json.load(f)
+    return {"data" : data}
+
+@app.route('/data_2017')
+def get_json():
+    f = open('json_data/json_2017')
+    data = json.load(f)
+    return {"data" : data}
+
+@app.route('/data_2018')
+def get_json():
+    f = open('json_data/json_2018')
+    data = json.load(f)
+    return {"data" : data}
+
+@app.route('/data_2019')
+def get_json():
+    f = open('json_data/json_2019')
+    data = json.load(f)
+    return {"data" : data}
 
 #If name is main, run flask
 if __name__ == '__main__':
