27929b6adc97263abbe6addad453a48e2299b70b
nkeplinger <keplinger.nicolette@gmail.com>
2023-02-26 12:18:32 -0600

    app file path update to json

--- app/app.py
+++ app/app.py
@@ -21,31 +21,31 @@ def test_data():
 
 @app.route('/data_2015')
 def get_json2015():
-    f = open('json_data/json_2015')
+    f = open('JSON_Data/json_2015')
     data2015 = json.load(f)
     return {"data" : data2015}
 
 @app.route('/data_2016')
 def get_json2016():
-    f = open('json_data/json_2016')
+    f = open('JSON_Data/json_2016')
     data2016 = json.load(f)
     return {"data" : data2016}
 
 @app.route('/data_2017')
 def get_json2017():
-    f = open('json_data/json_2017')
+    f = open('JSON_Data/json_2017')
     data2017 = json.load(f)
     return {"data" : data2017}
 
 @app.route('/data_2018')
 def get_json2018():
-    f = open('json_data/json_2018')
+    f = open('JSON_Data/json_2018')
     data2018 = json.load(f)
     return {"data" : data2018}
 
 @app.route('/data_2019')
 def get_json2019():
-    f = open('json_data/json_2019')
+    f = open('JSON_Data/json_2019')
     data2019 = json.load(f)
     return {"data" : data2019}
 
